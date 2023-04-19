359f04da6d667e6e4558abcf72de80b6358b3e08
Drew Payton <drewpayton18@gmail.com>
2023-04-19 09:07:05 -0500

    changed the server to a file and added a css with some starter code

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const PORT = 2023;
+const path = require("path")
 
 const app = express();
 
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"))
+})
 
 
 
